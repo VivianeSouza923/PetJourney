1292a36539104adf14230759ea558985287dca38
Viviane Souza <viviane.souza05@aluno.ifce.edu.br>
2024-01-31 10:01:25 -0300

    Adição do ClipRRect para a imagem de perfil do pet se adaptar ao container; Adição de listview para as opções do app

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,11 +1,10 @@
-
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
@@ -17,8 +16,8 @@ class HomePage extends StatelessWidget {
 
 class HomePageScreen extends StatefulWidget {
   const HomePageScreen({Key? key}) : super(key: key);
+
   @override
-  // ignore: library_private_types_in_public_api
   _HomePageScreenState createState() => _HomePageScreenState();
 }
 
@@ -31,17 +30,15 @@ class _HomePageScreenState extends State<HomePageScreen> {
     super.initState();
     _imagePicker = ImagePicker();
   }
-    // asíncrona
-    Future<void> _pickImage() async {
-      // esperaaa
-      final image = await _imagePicker.pickImage(source: ImageSource.gallery);
-      if (image != null) {
-        setState(() {
-          _imageFile = image;
-        });
-      }
+
+  Future<void> _pickImage() async {
+    final image = await _imagePicker.pickImage(source: ImageSource.gallery);
+    if (image != null) {
+      setState(() {
+        _imageFile = image;
+      });
     }
-  
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -60,7 +57,9 @@ class _HomePageScreenState extends State<HomePageScreen> {
         ),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: () {
+              // CRIAR A PÁGINA DE CONFIGURAÇÕES AINDA
+            },
             icon: const Icon(Icons.settings_outlined),
           )
         ],
@@ -71,51 +70,60 @@ class _HomePageScreenState extends State<HomePageScreen> {
             children: [
               Flexible(
                 child: Padding(
-                  padding: const EdgeInsets.only(left: 20, top: 20),
+                  padding: const EdgeInsets.only(left: 20, top: 30),
                   child: InkWell(
                     onTap: _pickImage,
                     child: Container(
                       height: 90,
                       width: 90,
-                                  
                       decoration: BoxDecoration(
                         color: Color(0xffd8bca4),
-                        borderRadius: BorderRadius.circular(30)
+                        borderRadius: BorderRadius.circular(35),
                       ),
-                      child:  _imageFile == null
-                                    ? Column(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.center,
-                                        children: [
-                                          Padding(
-                                            padding: EdgeInsets.symmetric(
-                                                horizontal: 24.0),
-                                            child: Text(
-                                              "Foto",
-                                              style: TextStyle(
-                                                fontFamily: 'FigTree',
-                                                fontSize: 16,
-                                                fontWeight: FontWeight.w400,
-                                                color: Colors.white,
-                                              ),
-                                            ),
-                                          ),
-                                          Icon(
-                                            Icons.upload,
-                                            color: Colors.white,
-                                          ),
-                                        ],
-                                      )
-                                    : Image.file(
-                                        File(_imageFile!.path),
-                                        fit: BoxFit.cover,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(35),
+                        child: _imageFile == null
+                            ? Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Padding(
+                                    padding:
+                                        EdgeInsets.symmetric(horizontal: 24.0),
+                                    child: Text(
+                                      "Foto",
+                                      style: TextStyle(
+                                        fontFamily: 'FigTree',
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.w400,
+                                        color: Colors.white,
                                       ),
+                                    ),
+                                  ),
+                                  Icon(
+                                    Icons.upload,
+                                    color: Colors.white,
+                                  ),
+                                ],
+                              )
+                            : Image.file(
+                                File(_imageFile!.path),
+                                fit: BoxFit.cover,
+                              ),
+                      ),
                     ),
                   ),
                 ),
               ),
             ],
           ),
+          SizedBox(
+            height: 40,
+          ),
+          
+          Expanded(child: ListView(
+            
+          ),
+          )
         ],
       ),
     );
